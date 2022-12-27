f239cb24f461ef9e46c04118f7806e37fef1c52a
lait <lait.zhang@gmail.com>
2022-12-27 15:58:50 +0800

    init

---
+++ log-boot/src/main/java/org/dows/log/boot/LogConfig.java
@@ -0,0 +1,12 @@
+package org.dows.log.boot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"org.dows.log"})
+@MapperScan(basePackages = {"org.dows.log.mapper"})
+public class LogConfig {
+
+}
---
+++ log-boot/src/main/java/org/dows/log/boot/aspect/LogAspect.java
@@ -0,0 +1,94 @@
+package org.dows.log.boot.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.dows.log.api.util.IpUtil;
+import org.dows.log.api.util.ThrowableUtil;
+import org.dows.log.entity.SysLog;
+import org.dows.log.service.LogService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Objects;
+
+@Component
+@Aspect
+@Slf4j
+public class LogAspect {
+
+    private final LogService logService;
+
+    ThreadLocal<Long> currentTime = new ThreadLocal<>();
+
+    public LogAspect(LogService logService) {
+        this.logService = logService;
+    }
+
+    /**
+     * 配置切入点
+     */
+    @Pointcut("@annotation(org.dows.log.api.annotation.Log)")
+    public void logPointcut() {
+        // 该方法无方法体,主要为了让同类中其他方法使用此切入点
+    }
+
+    /**
+     * 配置环绕通知,使用在方法logPointcut()上注册的切入点
+     *
+     * @param joinPoint join point for advice
+     */
+    @Around("logPointcut()")
+    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object result;
+        currentTime.set(System.currentTimeMillis());
+        result = joinPoint.proceed();
+        SysLog log = new SysLog("INFO", System.currentTimeMillis() - currentTime.get());
+        currentTime.remove();
+        HttpServletRequest request = getHttpServletRequest();
+        logService.save(getUsername(), IpUtil.getBrowser(request), IpUtil.getIp(request), joinPoint, log);
+        return result;
+    }
+
+    /**
+     * 配置异常通知
+     *
+     * @param joinPoint join point for advice
+     * @param e         exception
+     */
+    @AfterThrowing(pointcut = "logPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+        SysLog log = new SysLog("ERROR", System.currentTimeMillis() - currentTime.get());
+        currentTime.remove();
+        log.setExceptionDetail(ThrowableUtil.getStackTrace(e).getBytes());
+        HttpServletRequest request = getHttpServletRequest();
+        logService.save(getUsername(), IpUtil.getBrowser(request), IpUtil.getIp(request), (ProceedingJoinPoint) joinPoint, log);
+    }
+
+    public String getUsername() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null) {
+            //throw new BadRequestException(HttpStatus.UNAUTHORIZED, "当前登录状态过期");
+        }
+        if (authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return userDetails.getUsername();
+        }
+        //throw new BadRequestException(HttpStatus.UNAUTHORIZED, "找不到当前登录的信息");
+        return "";
+    }
+
+
+    private HttpServletRequest getHttpServletRequest() {
+        return ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
+    }
+}
