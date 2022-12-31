03b684f74ce5578603befd6cada39e15a4a61099
lait <lait.zhang@gmail.com>
2022-12-31 16:44:00 +0800

    fix

---
+++ log-boot/src/main/java/org/dows/log/boot/BinlogBeanProcessor.java
@@ -0,0 +1,38 @@
+package org.dows.log.boot;
+
+import org.dows.log.LogConfig;
+import org.dows.log.api.BinlogListener;
+import org.dows.log.core.MysqlListener;
+import org.dows.log.core.thread.BinlogThreadStarter;
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+
+@Component
+public class BinlogBeanProcessor implements SmartInitializingSingleton {
+    private ApplicationContext context;
+
+    @Autowired
+    private LogConfig logConfig;
+
+    public BinlogBeanProcessor(ApplicationContext context) {
+        this.context = context;
+    }
+
+    @Override
+    public void afterSingletonsInstantiated() {
+        Map<String, BinlogListener> beans = context.getBeansOfType(BinlogListener.class);
+
+        Map<String, List<MysqlListener>> listeners = beans.values().stream()
+                .map(MysqlListener::new)
+                .collect(Collectors.groupingBy(MysqlListener::getHostName));
+
+        listeners.forEach((k, v) -> new BinlogThreadStarter().runThread(logConfig.getByNameAndThrow(k), v));
+    }
+}
