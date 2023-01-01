b51f86818782d2c20b37a1e9013619136d26ce54
lait <lait.zhang@gmail.com>
2023-01-01 13:58:44 +0800

    fix

---
+++ log-api/src/main/java/org/dows/log/api/Database.java
@@ -0,0 +1,77 @@
+package org.dows.log.api;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * {
+ * "database": {
+ * "name": "test",
+ * "tables": [
+ * {
+ * "table": {
+ * "name": "",
+ * "comment": "",
+ * "columns": [
+ * {
+ * "name": "",
+ * "type": "",
+ * "length": "",
+ * "default": "",
+ * "comment": "",
+ * "unique": false,
+ * "index": {
+ * "flag": false,
+ * "seq": 1
+ * },
+ * "pk": {
+ * "flag": false,
+ * "strategy": "auto_inc"
+ * }
+ * }
+ * ]
+ * }
+ * }
+ * ]
+ * }
+ * }
+ */
+@Data
+public class Database {
+
+    private String name;
+    private List<Table> tables;
+
+
+    @Data
+    public static class Table {
+        private String name;
+        private String comment;
+        private List<Column> columns;
+    }
+
+    @Data
+    public static class Column {
+        private String name;
+        private String type;
+        private String comment;
+        private String defaultValue;
+        private String length;
+        private Boolean unique = false;
+        private Index index;
+        private PrimaryKey pk;
+    }
+
+    @Data
+    public static class Index {
+        private Boolean flag = false;
+        private Integer seq = 0;
+    }
+
+    @Data
+    public static class PrimaryKey {
+        private Boolean flag = false;
+        private String strategy;
+    }
+}
